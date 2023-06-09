6eafd5f4f1b8b0a8669bc1452959f18665a482c7
Darwin Galvan <darwin.galvan@insoftar.com>
2023-06-09 08:44:25 -0500

    avance de persistencia en fee_dateail

---
+++ domain/model/src/main/java/com/cls/model/entity/addfee/FeeDetailEntity.java
@@ -0,0 +1,39 @@
+package com.cls.model.entity.addfee;
+
+import lombok.Data;
+import org.hibernate.annotations.Immutable;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name="fee_detail")
+public class FeeDetailEntity {
+    @Id()
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "authorization_number")
+    private Integer authorizationNumber;
+    @Column(name = "fee_id")
+    private Integer feeId;
+    @Column(name = "fee_basic_price")
+    private Integer feeBasicPrice;
+    @Column(name = "fee_additional_price")
+    private Integer feeAdditionalPrice;
+    @Column(name = "fee_total")
+    private Integer feeTotal;
+    @Column(name = "fee_log")
+    private String feeLog;
+    @Column(name = "created_at")
+    @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    private Date createdAt;
+    @Column(name = "updated_at")
+    @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    private Date updatedAt;
+
+}
---
+++ domain/usecase/src/main/java/com/cls/domain/ports/addfee/FeeDetailRepositoryImpl.java
@@ -0,0 +1,12 @@
+package com.cls.domain.ports.addfee;
+
+import com.cls.domain.ports.addfee.out.FeeDetailRepository;
+import com.cls.model.entity.addfee.FeeDetailEntity;
+
+public class FeeDetailRepositoryImpl implements FeeDetailRepository {
+
+    @Override
+    public FeeDetailEntity addFeeDetail(FeeDetailEntity feeDetailEntity) {
+        return null;
+    }
+}
